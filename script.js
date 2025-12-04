25a6426f364923324f2c35fc88bdd006b2aef440
Moeenc137 <moeenc137@gmail.com>
2025-12-04 23:33:52 +0500

    completed
    Its not responsive yet, you can see in desktop mode if you are on mobile.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let btns = document.querySelectorAll(".button");
 let resetbtn = document.querySelector(".reset-btn");
-
+let resetbutton = () => {
+  turnO = true;
+  enablebox();
+  winnerPara.classList.add("hide");
+};
 let turnO = true;
 
 const winPatterns = [
@@ -24,5 +28,42 @@ btns.forEach((box) => {
       turnO = true;
     }
     box.disabled = true;
+    checkwinner();
   });
 });
+
+let disablebox = () => {
+  for (let box of btns) {
+    box.disabled = true;
+  }
+};
+let enablebox = () => {
+  for (let box of btns) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+let winnerPara = document.querySelector(".winner");
+let newBtn = document.querySelector(".new-btn");
+let showwinner = (winner) => {
+  winnerPara.innerText = `Winner is ${winner}`;
+  winnerPara.classList.remove("hide");
+  disablebox();
+};
+
+const checkwinner = () => {
+  for (pattern of winPatterns) {
+    let pos1 = btns[pattern[0]].innerText;
+    let pos2 = btns[pattern[1]].innerText;
+    let pos3 = btns[pattern[2]].innerText;
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        console.log("winner", pos1);
+        showwinner(pos1);
+      }
+    }
+  }
+};
+
+resetbtn.addEventListener("click", resetbutton);
