4123b49e24525e8cc04952f94564c07215c81f01
Moeenc137 <moeenc137@gmail.com>
2025-12-04 21:01:51 +0500

    in progress

---
+++ script.js
@@ -0,0 +1,28 @@
+let btns = document.querySelectorAll(".button");
+let resetbtn = document.querySelector(".reset-btn");
+
+let turnO = true;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+btns.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+  });
+});
